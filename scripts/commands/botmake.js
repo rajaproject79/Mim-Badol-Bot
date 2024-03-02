41fd50478666fecbd57d8909588c3f1dd56515d9
Devilling1111 <157110204+Devilling1111@users.noreply.github.com>
2024-03-02 19:25:08 +0600

    Create botmake.js

---
+++ scripts/commands/botmake.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+	name: "botmake",
+    version: "1.0.1",
+    permission: 0,
+    credits: "BADOL-KHAN",
+    prefix: true,
+    description: "ask any thing",
+    category: "admin",
+    usages: "",
+    cooldowns: 5,
+    dependencies: {
+        "openai": ""
+    }
+};
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
